eb4b61108b73583208ff71a43d2fb7978c105275
sam11385 <samluedke@gmail.com>
2017-12-28 08:49:37 -0500

    Add boilerplate html. Add some functional JS.

---
+++ js/for-loop.js
@@ -0,0 +1,19 @@
+const list = [
+  {
+    name: 'Don',
+  },
+  {
+    name: 'Ann',
+  },
+  {
+    name: 'Sally',
+  },
+  {
+    name: 'John',
+  },
+];
+
+for (let i = 0; i < list.length; i++) {
+  const person = list[i];
+  console.log(person.name);
+}
